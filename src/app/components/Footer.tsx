6d779c7e68fe474647920493557b4f407e5b9ed6
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-12-24 01:24:16 -0500

    finished init footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="w-[720px] h-auto p-10">
+      <p className="text-xs font-normal text-zinc-500 text-center">
+        2023 © Weiqi Zhuang
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
