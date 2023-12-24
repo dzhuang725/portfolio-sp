43e1dc0d24599736cf1da33880a2cb191cf35d76
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-12-24 01:23:10 -0500

    finished init hero section
    - need modify picture

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero-image">
+      <img
+        src="https://i.imgur.com/9C5cUow.jpg"
+        className="flex-shrink-0 w-56 h-56 rounded-full object-cover -mb-20"
+      />
+    </div>
+  );
+}
+
+export default Hero;
